7254c4f72774ee437cd2916005c97d518cea9c07
Ferdian Aria Finanta <110990246+gnferr@users.noreply.github.com>
2023-03-16 10:34:41 +0700

    nilaiupdate

---
+++ getnilai-mahasiswa.php
@@ -0,0 +1,12 @@
+<?php
+require_once "koneksi.php";
+
+$nim = $_GET['nim'];
+$myArray = array();
+if ($result = mysqli_query($conn, "SELECT * FROM nilaiakademik WHERE nim=$nim")) {
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $myArray[] = $row;
+    }
+    mysqli_close($conn);
+    echo json_encode($myArray);
+}
